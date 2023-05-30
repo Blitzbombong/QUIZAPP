ea296893446457d357894a516f212e5a6c001d78
Waldemar Giesbrecht <giesbrecht-83@web.de>
2023-05-30 21:35:14 +0200

    built-in functions

--- script.js
+++ script.js
@@ -86,21 +86,62 @@ let currentQuestinon = 0;
 
 function init(){
     document.getElementById('all-questions').innerHTML = questions.length;
+    
 
     showQuestion()
 }
 
 
 function showQuestion(){
+
+    if(currentQuestinon >= questions.length){
+        // TODO Show End Screen
+        document.getElementById('end-screen').style = '';
+        document.getElementById('question-card').style = 'display: none';
+    } else{
     let question = questions[currentQuestinon];
 
+    document.getElementById('next-level').innerHTML = currentQuestinon + 1;
     document.getElementById('questiontext').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 function answer(selection) {
-    
+    let question = questions[currentQuestinon]; /* hier befindet sich die aktuele Frage */
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        console.log('Richtige Antwort!!!');
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        console.log('Falsche Antwort!!!');
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+
+function nextQuestion() {
+    currentQuestinon++; // geht in die nechste Frage
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
